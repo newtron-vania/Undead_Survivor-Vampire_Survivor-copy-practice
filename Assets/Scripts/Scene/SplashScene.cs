f512f114738b41b02d739bf86993ee4b23697007
newtron-vania <galtab0403@gmail.com>
2023-01-19 02:23:29 +0900

    SplashScene Update

---
+++ Assets/Scripts/Scene/SplashScene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScene : MonoBehaviour
+{
+    float time = 0;
+    private void FixedUpdate()
+    {
+        time += Time.fixedDeltaTime;
+        if(time > 2.5)
+        {
+            SceneManager.LoadScene("MainMenuScene");
+        }
+    }
+}
